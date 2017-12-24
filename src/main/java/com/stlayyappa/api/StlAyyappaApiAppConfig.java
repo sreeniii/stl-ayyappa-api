75d8aeedb8614f23e62988360e29101c2ded7d3d
Sreenivas Kanthamneni <sreenivas.kanthamneni@monsanto.com>
2017-12-24 14:41:29 -0600

    added db connection

--- src/main/java/com/stlayyappa/api/StlAyyappaApiAppConfig.java
+++ src/main/java/com/stlayyappa/api/StlAyyappaApiAppConfig.java
@@ -1,7 +1,19 @@
 package com.stlayyappa.api;
 
+import org.springframework.boot.autoconfigure.jdbc.DataSourceBuilder;
+import org.springframework.boot.context.properties.ConfigurationProperties;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Primary;
+
+import javax.sql.DataSource;
 
 @Configuration
 public class StlAyyappaApiAppConfig {
+    @Bean
+    @Primary
+    @ConfigurationProperties(prefix = "spring.datasource")
+    public DataSource dataSource() {
+        return DataSourceBuilder.create().build();
+    }
 }
