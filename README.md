3a516aabd2d299ba2ffe81ea5202d25ac6889cf5
Sreenivas Kanthamneni <sreenivas.kanthamneni@monsanto.com>
2017-12-24 14:08:33 -0600

    first commit

---
+++ README.md
@@ -0,0 +1,1 @@
+# stl-ayyappa-api
---
+++ src/main/java/com/stlayyappa/api/StlAyyappaApiApp.java
@@ -0,0 +1,12 @@
+package com.stlayyappa.api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StlAyyappaApiApp {
+
+	public static void main(String[] args) {
+		SpringApplication.run(StlAyyappaApiApp.class, args);
+	}
+}
